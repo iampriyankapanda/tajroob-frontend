b2d428dbae7b52a6e1a2eaa8fe1612c397e3d995
Priyanka Panda <iampriyankapanda@gmail.com>
2022-04-03 20:18:09 +0530

    updated banner

--- src/assets/components/home-page/banner/index.js
+++ src/assets/components/home-page/banner/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React from "react";
 
 export default function HomePageBanner() {
-    return (
-
-        <div className="banner-wrapper d-flex">
-         <div className="banner-wrapper__elements text-center">
-         <h1 className="banner-wrapper__heading">Online Experiences</h1>
-         <p className="banner-wrapper__description">Find people with unique experiences and activities - all without leaving home</p>
-         <button className="theme-button theme-button__secondary">View all experiences</button>
-         </div>
+  return (
+    <div className="main-banner-wrapper">
+      <img
+        src="/images/home-page/banner/home-banner-image.jpeg"
+        className="img-fluid banner-image"
+      />
+      <div className="banner-wrapper">
+        <div className="banner-wrapper__elements text-center">
+          <h1 className="banner-wrapper__heading">Online Experiences</h1>
+          <p className="banner-wrapper__description">
+            Find people with unique experiences and activities - all without
+            leaving home
+          </p>
+          <button className="theme-button theme-button__secondary mb-3">
+            View all experiences
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
